a6a4df99b87a5568d289b7353dc950271fb450d8
HMHarris <HMHarris-git@github.com>
2021-02-09 15:29:24 +0530

    All Possible Combinations of a String or an Array.

---
+++ Combinations.py
@@ -0,0 +1,42 @@
+import time
+
+def combinations(arr):
+    n = len(arr)
+    # Result array to store all combinations
+    res=[]
+    for i in range(1,2**n):
+        '''
+        A new string variable to store individual combination and 
+        then reset for new iteration of outer loop
+        '''
+        data = ''
+        for j in range(0,n):
+            # This if statement will check if right most element in binary form of i is 1 or not
+            if (i&1): 
+                # if right-most is 1 the charcter will be added to data
+                data+=arr[j]
+            # Right-shift so that i so that if statement can check for each element in binary form of i    
+            i>>=1         
+        res.append(data)
+    return sorted(res)
+
+def starter():
+    print("xoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxox Choose you type of input xoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxox")
+    time.sleep(1)
+    print("Enter 1 for Array")
+    print("Enter 2 for String")
+    time.sleep(1)
+    choice = input("Enter here: ")
+    try:
+        if choice == '1':
+            arr = input("Enter your Array seperated by single space: \n").split()
+            print(combinations(arr))
+        elif choice == '2':
+            arr = input("Enter your string here: \n")
+            print(combinations(arr))
+    except:
+        raise Exception ("Invalid Input")
+
+
+if __name__ == "__main__":
+    starter()
